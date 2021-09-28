b48701fcf96b66afe3a5b07f46e9c1b78ec2f3b9
rahmathidayatmajid <rahmat@pajanlom.com>
2021-09-28 10:40:45 +0700

    + ready to deploy

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1', router)
+app.get('/', (req, res) => {
+    res.status(200).json({
+        status: "server running",
+        message: "success deploy project"
+    })
+})
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
